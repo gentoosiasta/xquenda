a3b9c1ddd0af1a551e7375920f93f6d83fb1ab58
Julio Aguilar Carmona <julio.aguilarc@gmail.com>
2025-12-11 13:51:04 -0600

    :card_file_box: Adds StudyPlan migration

---
+++ database/migrations/2025_12_11_194126_create_study_plans_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('study_plans', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('degree_program_id')->constrained('degree_programs');
+            $table->string('plan_code', 20)->nullable();
+            $table->date('start_date')->nullable();
+            $table->date('end_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('study_plans');
+    }
+};

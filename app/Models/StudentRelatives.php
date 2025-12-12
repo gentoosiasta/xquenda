08a5fd9ccf5238f9a60783af9d518e2430d6edc5
Julio Aguilar Carmona <julio.aguilarc@gmail.com>
2025-12-12 13:15:53 -0600

    :card_file_box: Adds StudentRelative migration

---
+++ app/Models/StudentRelatives.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class StudentRelatives extends Model
+{
+    /** @use HasFactory<\Database\Factories\StudentRelativesFactory> */
+    use HasFactory;
+}

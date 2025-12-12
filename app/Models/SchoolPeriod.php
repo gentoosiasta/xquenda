d04797176b1e11914aa73bd74f9eaaab73d353a1
Julio Aguilar Carmona <julio.aguilarc@gmail.com>
2025-12-12 09:30:44 -0600

    :card_file_box: Adds SchoolPeriod migration

---
+++ app/Models/SchoolPeriod.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SchoolPeriod extends Model
+{
+    /** @use HasFactory<\Database\Factories\SchoolPeriodFactory> */
+    use HasFactory;
+}
